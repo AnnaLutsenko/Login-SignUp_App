35abe28249c346298cfe8995a700662c7669db3f
Anna Lutsenko <nyuta.lutsenko@gmail.com>
2017-12-06 00:50:10 +0200

    added loginVC

---
+++ Login_4K-Soft/LoginViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LoginViewController.swift
+//  Login_4K-Soft
+//
+//  Created by Anna on 05.12.17.
+//  Copyright © 2017 Anna Lutsenko. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var imgLogo: UIImageView!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var enterBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    func initViewController() {
+        
+    }
+}
---
+++ Login_4K-Soft/Utilites/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  Login_4K-Soft
+//
+//  Created by Anna on 06.12.17.
+//  Copyright © 2017 Anna Lutsenko. All rights reserved.
+//
+
+import Foundation
+
+struct Constant {
+    
+    struct Color {
+        static let redError = "FC4D42"
+        static let blueBtn = "9DB5F0"
+        static let blueLightBtn = "CED9FB"
+        static let grayActiveBorderTF = "A9A9A9"
+        static let grayNotActiveTF = "F1F1F1"
+        static let grayTextInfo = "888888"
+        static let grayTxtPlaceholder = "707070"
+    }
+}
